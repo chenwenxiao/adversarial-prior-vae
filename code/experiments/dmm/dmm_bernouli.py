eb6caed53e74d5c95a18beff17843b4fb3852399
chenwenxiao <chenwenxiao@live.cn>
2019-06-28 17:27:04 +0800

    Minor update

--- code/experiments/dmm/dmm_bernouli.py
+++ code/experiments/dmm/dmm_bernouli.py
@@ -576,8 +576,8 @@ def main():
         dtype=tf.float32, shape=(None,) + config.x_shape, name='input_x')
     learning_rate = spt.AnnealingVariable(
         'learning_rate', config.initial_lr, config.lr_anneal_factor)
-    beta = tf.Variable(initial_value=config.beta, dtype=tf.float32, name='beta', trainable=True)
-    beta = tf.clip_by_value(beta, 0.0, 1.0)
+    beta = tf.Variable(initial_value=0.1, dtype=tf.float32, name='beta', trainable=True)
+    beta = tf.clip_by_value(beta, config.beta, 1.0)
 
     # derive the loss for initializing
     with tf.name_scope('initialization'), \
