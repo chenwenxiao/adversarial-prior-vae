01b2421739000ef4f97c79c4fa7f4ee570c1d967
chenwenxiao <chenwenxiao@live.cn>
2019-09-06 16:34:53 +0800

    Minor update

--- code/experiments/dmm/dmm_bernouli.py
+++ code/experiments/dmm/dmm_bernouli.py
@@ -36,7 +36,7 @@ spt.Bernoulli.mean = property(_bernoulli_mean)
 
 class ExpConfig(spt.Config):
     # model parameters
-    z_dim = 64
+    z_dim = 40
     act_norm = False
     weight_norm = False
     l2_reg = 0.0002
@@ -1008,7 +1008,7 @@ def main():
         # elif config.z_dim == 3072:
         #     restore_checkpoint = '/mnt/mfs/mlstorage-experiments/cwx17/5d/19/6f9d69b5d1936fb2d2d5/checkpoint/checkpoint/checkpoint.dat-390000'
         # else:
-        restore_checkpoint = '/mnt/mfs/mlstorage-experiments/cwx17/ff/19/6f3b6c3ef49d9fd766d5/checkpoint/checkpoint/checkpoint.dat-58000'
+        restore_checkpoint = None
 
         # train the network
         with spt.TrainLoop(tf.trainable_variables(),
