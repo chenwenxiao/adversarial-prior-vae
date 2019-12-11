c8587a6420a6cde7ac5b6600ad57c469112b5f37
aaalll3 <lwd12345678901234@163.com>
2019-12-11 17:27:27 +0800

    split train/test set

--- code/experiments/datasets/celeba.py
+++ code/experiments/datasets/celeba.py
@@ -1,3 +1,13 @@
+'''
+load CelebA dataset as numpy array
+
+usage:
+
+    import celeba
+
+    (train_x, train_y), (test_x, test_y) = load_celeba()
+
+'''
 import requests
 import zipfile,os
 import tfsnippet as spt
@@ -7,6 +17,7 @@ import numpy as np
 import matplotlib.image as mpimg
 import random
 import cv2 as cv
+import shutil
 
 
 def download_file_from_google_drive(id, destination):
@@ -63,12 +74,36 @@ class misc():
             os.makedirs(dest)
         f.extractall(dest)  
 
+    celba_size = 202598;
+    @staticmethod
+    def get_tt():
+        train=[]
+        test=[]
+        for i in range(202598):
+            random.seed(i)
+            if (random.random()>0.83333):
+                test.append(i)
+            else:
+                train.append(i)
+        return train,test
+
 DEBUG_IMG = '/Users/lwd/Downloads/img_align_celeba'
 DEBUG_EVAL = '/Users/lwd/Downloads/list_eval_partition.txt'
 
 IMG_ZIP_PATH = '/home/cwx17/data/celeba/img_align_celeba.zip'
 IMG_PATH = '/home/cwx17/data/celeba/img_align_celeba'
 EVAL_PATH = '/home/cwx17/data/celeba/eval.txt'
+
+TRAIN_DIR_PATH = '/home/cwx17/data/celeba/train'
+TRAIN_X_PATH = '/home/cwx17/data/celeba/train/img'
+TRAIN_Y_PATH = '/home/cwx17/data/celeba/train/train_eval.txt'
+TRAIN_INFO_PATH = '/home/cwx17/data/celeba/train/info.txt'
+
+
+TEST_DIR_PATH = '/home/cwx17/data/celeba/test'
+TEST_X_PATH = '/home/cwx17/data/celeba/test/img'
+TEST_Y_PATH = '/home/cwx17/data/celeba/test/test_eval.txt'
+TEST_INFO_PATH = '/home/cwx17/data/celeba/test/info.txt'
 
 def _fetch_array_x(path):
     file_names = os.listdir(path)
@@ -117,6 +152,24 @@ def _fetch_array_y(path):
                 evalue.append(q)
     return np.array(evalue)
 
+def _store_array_x(arr,path,prefix):
+    cnt=1;
+    if not os.path.exists(path):
+        os.makedirs(path)
+    for img in arr:
+        mpimg.imsave(f'{path}/{prefix}{cnt}.jpg',img)
+        cnt+=1
+
+def _store_array_y(arr,path,dir_path,prefix):
+    cnt = 1;
+    if not os.path.exists(path):
+        os.makedirs(path)
+    with open(path,'w+') as f:
+        for i in arr:
+            f.write(f"{prefix}{cnt} {i}\n")
+            cnt+=1
+        
+
 debug = False
 
 
@@ -138,50 +191,76 @@ def load_celeba(x_shape=(218, 178), x_dtype=np.float32, y_dtype=np.int32,
             (train_x, train_y), (test_x, test_y)
             
     """
-    if debug:
-        if not os.path.exists(DEBUG_EVAL):
-            print(f'eval doesn\'t exist\ndownloading eval \ndst: {DEBUG_EVAL}')
-            misc.download_celeba_eval(DEBUG_EVAL);
-            print('downloaded')
-        x = _fetch_array_x(DEBUG_IMG).astype(x_dtype)
-        y = _fetch_array_y(DEBUG_EVAL).astype(y_dtype)
-    else:   
-        if not os.path.exists(IMG_PATH):
-            print('img not exist')
-            if os.path.exists(IMG_ZIP_PATH):
-                print(f'zipped file exists\n unzipping\ndst: {IMG_PATH}')
-                misc.unzip(IMG_ZIP_PATH,IMG_PATH)
-                print('unzipped')
-            else:
-                print(f'zipped file dosen\'t exist\ndownloading img \ndst: {IMG_ZIP_PATH}')
-                misc.download_celeba_img(IMG_ZIP_PATH);
-                print(f'downloaded\nstart unzip\ndst: {IMG_PATH}')
-                misc.unzip(IMG_ZIP_PATH,IMG_PATH)
-                print('unzipped')
-        if not os.path.exists(EVAL_PATH):
-            print(f'eval doesn\'t exist\ndownloading eval \ndst: {EVAL_PATH}')
-            misc.download_celeba_eval(EVAL_PATH);
-            print('downloaded')
+    if (not os.path.exists(TRAIN_INFO_PATH)) or (not os.path.exists(TEST_INFO_PATH)):
+        print('train/test info doesn\'t exist')
+        train,test = misc.get_tt()
+        shutil.rmtree(TRAIN_DIR_PATH)
+        shutil.rmtree(TEST_DIR_PATH)
+        os.makedirs(TRAIN_DIR_PATH)
+        os.makedirs(TEST_DIR_PATH)
+        with open(TRAIN_INFO_PATH,'w+') as f:
+            for i in train:
+                f.write(i)
+        with open(TEST_INFO_PATH,'w+') as f:
+            for i in test:
+                f.write(i)
 
-        x = _fetch_array_x(IMG_PATH).astype(x_dtype)
-        y = _fetch_array_y(EVAL_PATH).astype(y_dtype)
+        if debug:
+            if not os.path.exists(DEBUG_EVAL):
+                print(f'eval doesn\'t exist\ndownloading eval \ndst: {DEBUG_EVAL}')
+                misc.download_celeba_eval(DEBUG_EVAL);
+                print('downloaded')
+            x = _fetch_array_x(DEBUG_IMG).astype(x_dtype)
+            y = _fetch_array_y(DEBUG_EVAL).astype(y_dtype)
+        else:   
+            if not os.path.exists(IMG_PATH):
+                print('img not exist')
+                if os.path.exists(IMG_ZIP_PATH):
+                    print(f'zipped file exists\n unzipping\ndst: {IMG_PATH}')
+                    misc.unzip(IMG_ZIP_PATH,IMG_PATH)
+                    print('unzipped')
+                else:
+                    print(f'zipped file dosen\'t exist\ndownloading img \ndst: {IMG_ZIP_PATH}')
+                    misc.download_celeba_img(IMG_ZIP_PATH);
+                    print(f'downloaded\nstart unzip\ndst: {IMG_PATH}')
+                    misc.unzip(IMG_ZIP_PATH,IMG_PATH)
+                    print('unzipped')
+            if not os.path.exists(EVAL_PATH):
+                print(f'eval doesn\'t exist\ndownloading eval \ndst: {EVAL_PATH}')
+                misc.download_celeba_eval(EVAL_PATH);
+                print('downloaded')
 
-    train_x = []
-    train_y = []
-    test_x = []
-    test_y = []
-    for i in range(len(x)):
-        if random.random()<0.8 :
-            train_x.append(x[i])      
+            x = _fetch_array_x(IMG_PATH).astype(x_dtype)
+            y = _fetch_array_y(EVAL_PATH).astype(y_dtype)
+
+        train_x = []
+        train_y = []
+        test_x = []
+        test_y = []
+
+        for i in train:
+            train_x.append(x[i])
             train_y.append(y[i])
-        else:
-            test_x.append(x[i])      
+
+        for i in test:
+            test_x.append(x[i])
             test_y.append(y[i])
 
-    train_x = np.array(train_x)
-    train_y = np.array(train_y)
-    test_x = np.array(test_x)
-    test_y = np.array(test_y)
+        train_x = np.array(train_x)
+        train_y = np.array(train_y)
+        test_x = np.array(test_x)
+        test_y = np.array(test_y)
+
+        _store_array_x(train_x,TRAIN_X_PATH,'train')
+        _store_array_y(train_y,TRAIN_Y_PATH, TRAIN_DIR_PATH,'train')
+        _store_array_x(test_x,TEST_X_PATH,'test')
+        _store_array_y(test_y,TEST_Y_PATH, TEST_DIR_PATH,'test')
+    else:
+        train_x = _fetch_array_x(TRAIN_X_PATH)
+        train_y = _fetch_array_y(TRAIN_Y_PATH)
+        test_x = _fetch_array_x(TEST_X_PATH)
+        test_y = _fetch_array_y(TEST_Y_PATH)
+    
 
     if normalize_x:
         train_x /= np.asarray(255., dtype=x.dtype)
