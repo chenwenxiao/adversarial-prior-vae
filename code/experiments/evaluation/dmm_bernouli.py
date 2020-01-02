49adb4e38e85165cac1ad62905c3b26de910b5bb
chenwenxiao <chenwenxiao@live.cn>
2020-01-02 19:29:57 +0800

    Minor update

--- code/experiments/evaluation/dmm_bernouli.py
+++ code/experiments/evaluation/dmm_bernouli.py
@@ -1163,32 +1163,31 @@ def main():
             # adversarial training
             for epoch in epoch_iterator:
 
-                if epoch % config.test_epoch_freq == 0:
-                    with loop.timeit('compute_Z_time'):
-                        # log_Z_list = []
-                        # for i in range(config.log_Z_times):
-                        #     log_Z_list.append(session.run(log_Z_compute_op))
-                        # from scipy.misc import logsumexp
-                        # log_Z = logsumexp(np.asarray(log_Z_list)) - np.log(len(log_Z_list))
-                        # print('log_Z_list:{}'.format(log_Z_list))
-                        # print('log_Z:{}'.format(log_Z))
+                with loop.timeit('compute_Z_time'):
+                    # log_Z_list = []
+                    # for i in range(config.log_Z_times):
+                    #     log_Z_list.append(session.run(log_Z_compute_op))
+                    # from scipy.misc import logsumexp
+                    # log_Z = logsumexp(np.asarray(log_Z_list)) - np.log(len(log_Z_list))
+                    # print('log_Z_list:{}'.format(log_Z_list))
+                    # print('log_Z:{}'.format(log_Z))
 
-                        log_Z_list = []
-                        for [batch_x, batch_origin_x] in train_flow:
-                            log_Z_list.append(session.run(another_log_Z_compute_op, feed_dict={
-                                input_x: batch_x,
-                                input_origin_x: batch_origin_x
-                            }))
-                        from scipy.misc import logsumexp
-                        another_log_Z = logsumexp(np.asarray(log_Z_list)) - np.log(len(log_Z_list))
-                        # print('log_Z_list:{}'.format(log_Z_list))
-                        print('another_log_Z:{}'.format(another_log_Z))
-                        # final_log_Z = logsumexp(np.asarray([log_Z, another_log_Z])) - np.log(2)
-                        final_log_Z = another_log_Z  # TODO
-                        get_log_Z().set(final_log_Z)
+                    log_Z_list = []
+                    for [batch_x, batch_origin_x] in train_flow:
+                        log_Z_list.append(session.run(another_log_Z_compute_op, feed_dict={
+                            input_x: batch_x,
+                            input_origin_x: batch_origin_x
+                        }))
+                    from scipy.misc import logsumexp
+                    another_log_Z = logsumexp(np.asarray(log_Z_list)) - np.log(len(log_Z_list))
+                    # print('log_Z_list:{}'.format(log_Z_list))
+                    print('another_log_Z:{}'.format(another_log_Z))
+                    # final_log_Z = logsumexp(np.asarray([log_Z, another_log_Z])) - np.log(2)
+                    final_log_Z = another_log_Z  # TODO
+                    get_log_Z().set(final_log_Z)
 
-                    with loop.timeit('eval_time'):
-                        evaluator.run()
+                with loop.timeit('eval_time'):
+                    evaluator.run()
 
                 loop.collect_metrics(lr=learning_rate.get())
                 loop.print_logs()
