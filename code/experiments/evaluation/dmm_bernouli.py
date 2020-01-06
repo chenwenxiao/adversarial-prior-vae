adb221349774c230f7768314a3182cb0263b65e7
chenwenxiao <chenwenxiao@live.cn>
2020-01-06 21:21:16 +0800

    Minor update

--- code/experiments/evaluation/dmm_bernouli.py
+++ code/experiments/evaluation/dmm_bernouli.py
@@ -37,7 +37,7 @@ spt.Bernoulli.mean = property(_bernoulli_mean)
 
 class ExpConfig(spt.Config):
     # model parameters
-    z_dim = 12
+    z_dim = 13
     act_norm = False
     weight_norm = False
     l2_reg = 0.0002
