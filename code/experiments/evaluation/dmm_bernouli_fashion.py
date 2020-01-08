445eb515782a83efb0269962c474462cd02f8752
chenwenxiao <chenwenxiao@live.cn>
2020-01-09 01:58:28 +0800

    Minor update

--- code/experiments/evaluation/dmm_bernouli_fashion.py
+++ code/experiments/evaluation/dmm_bernouli_fashion.py
@@ -972,7 +972,7 @@ def main():
         # elif config.z_dim == 3072:
         #     restore_checkpoint = '/mnt/mfs/mlstorage-experiments/cwx17/5d/19/6f9d69b5d1936fb2d2d5/checkpoint/checkpoint/checkpoint.dat-390000'
         # else:
-        restore_checkpoint = '/mnt/mfs/mlstorage-experiments/cwx17/19/0c/d4e63c432be9da0ec0e5/checkpoint/checkpoint/checkpoint.dat-936000'
+        restore_checkpoint = '/mnt/mfs/mlstorage-experiments/cwx17/b9/1c/d445f4f80a9f8ab3c0e5/checkpoint/checkpoint/checkpoint.dat-936000'
         # train the network
         with spt.TrainLoop(tf.trainable_variables(),
                            var_groups=['q_net', 'p_net', 'posterior_flow', 'G_theta', 'D_psi', 'G_omega', 'D_kappa'],
