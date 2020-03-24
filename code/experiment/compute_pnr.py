fcc609595e23e2f84f97c87dfb6cbb8c39cd43af
aaalll3 <lwd12345678901234@163.com>
2020-03-24 20:24:05 +0800

    added savez

--- code/experiment/compute_pnr.py
+++ code/experiment/compute_pnr.py
@@ -5,7 +5,7 @@ import tfsnippet as spt
 import numpy as np
 import matplotlib.pyplot as plt
 
-def compute(img_path,dataset_name):
+def compute(img_path,dataset_name,savename=''):
     dataset={'mnist':spt.datasets.load_mnist,
              'cifar10':spt.datasets.load_cifar10,
              'fashion':spt.datasets.load_fashion_mnist,
@@ -23,8 +23,8 @@ def compute(img_path,dataset_name):
         real_images = np.concatenate((train_x,test_x),axis=0)
     limit=min(len(gen_images),len(train_x))
     limit=min(50000,limit)
-    limit=100
-    base=43304
+    # limit=100
+    base=0
     real_images=real_images[base:base+limit]
     gen_images=gen_images[base:base+limit]
     print(gen_images[12])
@@ -34,8 +34,9 @@ def compute(img_path,dataset_name):
     print('1 precision ',result[0],' recall ',result[1])
     print('2 precision ',result[2],'\nrecall ',result[3])
     utils.plot([(result[2],result[3])],name=dataset_name)
+    np.savez(f'./{savename}-{dataset_name}',precision=result[2],recall=result[3])
     # for i in result[2]:
-    #     print(i,end='')
+    #     print(i,end ='')
     # for i in result[3]:
     #     print(i,end='')
 
